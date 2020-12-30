08e0ff6a49fab4b9f6940cab13f9a39abeb97d40
unya33ki <iimuzoku2@yahoo.co.jp>
2020-12-30 17:04:17 +0900

    deviceの導入とusersテーブルの作成

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "WorkShare_production"
+  # config.active_job.queue_name_prefix = "WorkProgress_production"
 
   config.action_mailer.perform_caching = false
 
