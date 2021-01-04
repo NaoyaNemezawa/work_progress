494520da98493e5589553b4e22cf7115d4468227
unya33ki <iimuzoku2@yahoo.co.jp>
2021-01-04 20:17:28 +0900

    タスク保存機能の作成。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,24 @@
 class TasksController < ApplicationController
+  before_action :project_find
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to project_path(params[:project_id])
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name,:specifilcs).merge(project_id: params[:project_id])
+  end
+
+  def project_find
+    @project = Project.find(params[:project_id])
   end
 end
