1a34340dd396ae170f3c84c4b3f56518d3bd41d8
unya33ki <iimuzoku2@yahoo.co.jp>
2021-01-06 15:24:19 +0900

    Taskモデルのテストコード作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
-    
+    name {"テスト用タスクネーム"}
+    specifics {"テスト用タスク詳細"}
+    association :project
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @task = FactoryBot.build(:task)
+  end
+
+  describe "タスク作成機能" do
+    context "新規タスクを作成できるとき" do
+      it "name,specificsがあれば作成できる" do
+        expect(@task).to be_valid
+      end
+
+      it "specificsがなくても作成できる" do
+        @task.specifics = ""
+        expect(@task).to be_valid
+      end
+    end
+
+    context "新規タスクを作成できないとき" do
+      it "nameがないと作成できない" do
+        @task.name = ""
+        @task.valid?
+        expect(@task.errors.full_messages).to include("Name can't be blank")
+      end
+    end
+  end
 end
