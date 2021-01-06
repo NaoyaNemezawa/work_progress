e3a30f972508d32775d945f3de9e3922775ec1b9
unya33ki <iimuzoku2@yahoo.co.jp>
2021-01-06 15:43:08 +0900

    Tasksコントローラーのテストコード作成

--- spec/requests/tasks_request_spec.rb
+++ spec/requests/tasks_request_spec.rb
@@ -1,11 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "Tasks", type: :request do
+RSpec.describe TasksController, type: :request do
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/tasks/new"
-      expect(response).to have_http_status(:success)
+  before do
+    @user = FactoryBot.create(:user)
+    @project = FactoryBot.create(:project)
+    @task = FactoryBot.create(:task)
+    sign_in @user
+  end
+  describe "GET #show" do
+    context "ログインしているとき" do
+      it "showアクションにリクエストすると正常にレスポンスが返ってくる" do
+        get project_task_path(@project.id,@task.id)
+        expect(response.status).to eq 200
+      end
+
+      it "showアクションにリクエストするとレスポンスにタスク名が含まれる" do
+        get project_task_path(@project.id,@task.id)
+        expect(response.body).to include @task.name
+      end
+
+      it "showアクションにリクエストするとレスポンスにプロジェクト名が含まれる" do
+        get project_task_path(@project.id,@task.id)
+        expect(response.body).to include @project.name
+      end
+    end
+
+    context "ログアウト状態の場合" do
+      it "ログアウト状態ではログインページにリダイレクトする" do
+        sign_out @user
+        get project_task_path(@project.id,@task.id)
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 
