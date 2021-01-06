7813625d4b1243ae95b6d3bd26f72d33f90b507e
unya33ki <iimuzoku2@yahoo.co.jp>
2021-01-06 18:57:16 +0900

    タスクへの進捗コメント投稿機能作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,21 @@ class CommentsController < ApplicationController
   def index
     @project = Project.find(params[:project_id])
     @task = Task.find(params[:task_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    binding.pry
+    if @comment.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, task_id: params[:task_id])
   end
 end
