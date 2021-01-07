7b0a329c0316f88fdf21910584ba1a0eac697487
unya33ki <iimuzoku2@yahoo.co.jp>
2021-01-07 12:33:04 +0900

    コメント投稿、表示機能作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :find
   def index
-    @project = Project.find(params[:project_id])
-    @task = Task.find(params[:task_id])
     @comment = Comment.new
   end
 
   def create
     @comment = Comment.new(comment_params)
-    binding.pry
     if @comment.save
       redirect_to root_path
     else
@@ -18,5 +16,11 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:comment).merge(user_id: current_user.id, task_id: params[:task_id])
+  end
+
+  def find
+    @project = Project.find(params[:project_id])
+    @task = Task.find(params[:task_id])
+    @comments = @task.comments.includes(:user)
   end
 end
