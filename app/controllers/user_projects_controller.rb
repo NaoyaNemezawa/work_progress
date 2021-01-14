aebcd4631dbda0a43c086a132c027789edd28797
unya33ki <iimuzoku2@yahoo.co.jp>
2021-01-14 13:02:11 +0900

    メンバー招待をドロップダウン形式に変更

--- app/controllers/user_projects_controller.rb
+++ app/controllers/user_projects_controller.rb
@@ -5,13 +5,14 @@ class UserProjectsController < ApplicationController
   end
 
   def create
+    # binding.pry
     @user_project = UserProject.new
-    user = User.find_by(email: params[:user_project][:email])
+    user = User.find_by(email: params[:email])
     @user_project[:project_id] = params[:project_id]
     unless user.nil?
       @user_project[:user_id] = user.id
       if @user_project.save
-        redirect_to project_path(params[:project_id])
+        redirect_to project_tasks_path(params[:project_id])
       else
         render :new
       end
