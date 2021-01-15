1a4b012b8d0bf0ac5c64e2c69f3d014bdf58062d
unya33ki <iimuzoku2@yahoo.co.jp>
2021-01-15 18:31:01 +0900

    コメント画面を部分テンプレートから独立。
    ifで表示内容を変えていたが、コメント画面以外で共有しないため独立。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
   end
 
   def create
+    binding.pry
     @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to index_comments
