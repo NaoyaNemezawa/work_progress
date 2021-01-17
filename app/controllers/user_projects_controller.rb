d4dc9f04758ad401d216a82325a506e539889b13
unya33ki <iimuzoku2@yahoo.co.jp>
2021-01-17 18:07:10 +0900

    エラーメッセージ追加

--- app/controllers/user_projects_controller.rb
+++ app/controllers/user_projects_controller.rb
@@ -2,6 +2,7 @@ class UserProjectsController < ApplicationController
   before_action :find_data
 
   def create
+    flash.now[:error] = []
     @user_project = UserProject.new
     user = User.find_by(email: params[:user_project][:email])
     @user_project[:project_id] = params[:project_id]
@@ -10,9 +11,11 @@ class UserProjectsController < ApplicationController
       if @user_project.save
         redirect_to project_tasks_path(params[:project_id])
       else
+        flash.now[:error] << "#{user.name}はすでにメンバーにいます。"
         render "tasks/index"
       end
     else
+      flash.now[:error] << "#{params[:user_project][:email]}は登録されていません。"
       render "tasks/index"
     end
   end
