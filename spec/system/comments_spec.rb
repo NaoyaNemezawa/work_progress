3f31c812d1e6c7c2115727edd225498e8c86bb9f
unya33ki <iimuzoku2@yahoo.co.jp>
2021-01-26 16:22:30 +0900

    コメント削除機能の結合テスト

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -59,7 +59,7 @@ RSpec.describe "コメント編集機能", type: :system do
       sign_in(@user)
       click_on @project.name
       click_on @task.name
-      # コメント作成画面の確認
+      # コメント編集画面の確認
       find_by_id("ce#{@comment.id}").click
       expect(page).to have_content "#{@comment.comment}を変更します"
       # コメントを編集してもコメントテーブルのカウントは変わらない。
@@ -75,15 +75,6 @@ RSpec.describe "コメント編集機能", type: :system do
       # ログインしていない状態でURLを直接入力するとログインページへ遷移する
       visit project_task_comments_path(@project.id, @task.id)
       expect(current_path).to eq new_user_session_path
-    end
-    it "招待されていないユーザーはコメントを作成できない" do
-      user2 = FactoryBot.create(:user)
-      sign_in(user2)
-      # 招待されていないユーザーはプロジェクトのリンクが存在しない
-      expect(page).to have_no_link @project.name
-      pending "要対策。URLを直接入力するとインデックスへ遷移する"
-      visit project_task_comments_path(@project.id, @task.id)
-      expect(current_path).to eq root_path
     end
     it "コメントを投稿したユーザー以外はコメントの編集ができない" do
       # メンバーの招待
@@ -102,4 +93,53 @@ RSpec.describe "コメント編集機能", type: :system do
     end
   end
 end
-# binding.pry
+
+RSpec.describe "コメント削除機能", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @project = FactoryBot.create(:project)
+    UserProject.create(user_id:@user.id, project_id:@project.id)
+    Task.create(name: "テスト用タスク", specifics: nil, project_id:@project.id)
+    @task = Task.find_by(project_id:@project.id)
+    Comment.create(comment:"テスト用コメント",task_id:@task.id,user_id:@user.id)
+    @comment = Comment.find_by(task_id:@task.id)
+  end
+  context "コメント削除できるとき" do
+    it "コメントしたユーザーはコメントを削除できる" do
+      # ログインしてコメント一覧を表示する
+      sign_in(@user)
+      click_on @project.name
+      click_on @task.name
+      # コメント画面の確認
+      find_by_id("cd#{@comment.id}").click
+      expect(page).to have_content "#{@comment.comment}を削除します"
+      # コメントを削除するとコメントテーブルのカウントが１下がる。
+      expect{find_link("削除").click}.to change{Comment.count}.by(-1)
+      # コメント一覧画面に削除したコメントが表示されない
+      expect(current_path).to eq project_task_comments_path(@project.id, @task.id)
+      expect(page).to have_no_content "テスト用コメントの編集"
+    end
+  end
+  context "コメントの削除ができないとき" do
+    it "ログインしていないとコメントの編集ができない" do
+      # ログインしていない状態でURLを直接入力するとログインページへ遷移する
+      visit project_task_comments_path(@project.id, @task.id)
+      expect(current_path).to eq new_user_session_path
+    end
+    it "コメントを投稿したユーザー以外はコメントの削除ができない" do
+      # メンバーの招待
+      user2 = FactoryBot.create(:user)
+      UserProject.create(user_id:user2.id,project_id:@project.id)
+      # ログインしてコメント一覧を表示
+      sign_in(user2)
+      click_on @project.name
+      click_on @task.name
+      # コメント表示の確認
+      expect(page).to have_content @comment.comment
+      pending "要対策。編集ボタンの非表示"
+      within "#comment#{@comment.id}" do
+        expect(page).to have_no_content("削除")
+      end
+    end
+  end
+end
