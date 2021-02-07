ea05841a41b8f22c0d30e51011e272685a050a63
unya33ki <iimuzoku2@yahoo.co.jp>
2021-02-07 15:56:17 +0900

    保存する画像サイズの指定

--- app/uploaders/img_uploader.rb
+++ app/uploaders/img_uploader.rb
@@ -1,7 +1,7 @@
 class ImgUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -23,6 +23,7 @@ class ImgUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
+  process resize_to_limit: [400, 600]
   #
   # def scale(width, height)
   #   # do something
@@ -36,7 +37,7 @@ class ImgUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_whitelist
-  #   %w(jpg jpeg gif png)
+    # %w(jpg jpeg gif png)
   # end
 
   # Override the filename of the uploaded files:
