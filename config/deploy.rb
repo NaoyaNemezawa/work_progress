9fcc2e801832e7f7a525424be1720dfd7a9e42e4
unya33ki <iimuzoku2@yahoo.co.jp>
2021-02-15 21:03:56 +0900

    自動デプロイ再設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,22 @@
 lock "~> 3.15.0"
 
+# Capistranoのログの表示に利用する
 set :application, "work_progress"
+# どのリポジトリからアプリをpullするかを指定する
 set :repo_url, "git@github.com:NaoyaNemezawa/work_progress.git"
 
+# バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5' 
 
-set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/catvitamin2256.pem'] 
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],keys: ['~/.ssh/catvitamin2256.pem'] 
 
+# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
+# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
