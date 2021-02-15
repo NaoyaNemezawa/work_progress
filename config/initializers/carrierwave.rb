e3b35c4781d2646694070c5a2937bf8b430e5fd3
unya33ki <iimuzoku2@yahoo.co.jp>
2021-02-15 13:14:26 +0900

    本番環境の記述を一旦コメントアウト
    これで本番環境も変わるかなぁ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,19 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_credentials = {
-      provider: "AWS",
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"] ,
-      aws_secret_access_key_id: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: "ap-northeast-1"
-    }
-    config.fog_directory = "progress226"
-    config.cache_storage = :fog
-  else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
-  end
-end
+# CarrierWave.configure do |config|
+#   if Rails.env.production?
+#     config.fog_credentials = {
+#       provider: "AWS",
+#       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"] ,
+#       aws_secret_access_key_id: ENV["AWS_SECRET_ACCESS_KEY"],
+#       region: "ap-northeast-1"
+#     }
+#     config.fog_directory = "progress226"
+#     config.cache_storage = :fog
+#   else
+#     config.storage :file
+#     config.enable_processing = false if Rails.env.test?
+#   end
+# end
