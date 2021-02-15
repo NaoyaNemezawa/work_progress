f503536d42e416b875c98fe8484eeec3309c4d51
unya33ki <iimuzoku2@yahoo.co.jp>
2021-02-15 21:59:08 +0900

    まずはローカルからs３へ保存できる？

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  # if Rails.env.production?
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id] ,
@@ -12,8 +12,9 @@ CarrierWave.configure do |config|
     }
     config.fog_directory = "progress226"
     config.cache_storage = :fog
-  else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
-  end
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/progress226'
+  # else
+  #   config.storage :file
+  #   config.enable_processing = false if Rails.env.test?
+  # end
 end
